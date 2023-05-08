d566933988a49b580af34f9851c8d880621a3c6a
Alan Vargas <112961710+AlanEzequielVargas@users.noreply.github.com>
2023-05-08 19:21:49 -0300

    cart en progreso , servicio de filtrado por categoria terminado , swiper card instalado y utilizado

--- nyka-ecommerce/src/components/Cart.jsx
+++ nyka-ecommerce/src/components/Cart.jsx
@@ -1,15 +1,47 @@
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
 
+import Link from "next/link";
+import CardCart from "./cards/CardCart";
+import InputPostalCode from "./inputs/InputPostalCode";
+import CardPaymentMethod from "./cards/CardPaymentMethod";
+import { IconCloseSvg } from "./Svgs";
+
 const Cart = ({ closeCartSideBar, open }) => {
   return (
-    <Menu isOpen={open} right>
+    <Menu isOpen={false} right width={600}>
       <div
-        className={`h-96 w-96 flex flex-col py-6 bg-white shadow-xl overflow-y-scroll ${
+      id="without-scroll"
+        className={`h-screen p-6 bg-white shadow-xl rounded-l-lg overflow-scroll text-black ${
           open ? "" : ""
         }`}
       >
-       
+        <div className="flex flex-col gap-5 ">
+          <div className="flex items-center justify-between">
+          <h2 className="font-normal text-3xl">Carrito de compras</h2>
+          <IconCloseSvg/>
+          </div>
+          <CardCart />
+          <CardCart />
+          <CardCart />
+          <button className="font-bold text-lg border rounded p-2 shadow-sm w-56 m-auto">
+            Agregar mas productos
+          </button>
+          <h3 className="font-bold text-lg">Calcular envío</h3>
+          <div>
+            <div className="flex items-center gap-3">
+              <InputPostalCode />
+              <Link href="#" className="underline block">
+                Consultar mi código postal
+              </Link>
+            </div>
+          </div>
+          <h3 className="font-bold text-lg">Método de pago</h3>
+          <div className="flex flex-col gap-4 font-bold">
+            <CardPaymentMethod />
+            <CardPaymentMethod />
+          </div>
+        </div>
       </div>
     </Menu>
   );
--- nyka-ecommerce/src/components/layout/Layout.jsx
+++ nyka-ecommerce/src/components/layout/Layout.jsx
@@ -1,15 +1,15 @@
-
-import React from 'react';
-import NavBar from '../NavBar';
-import Footer from '../Footer';
-import Cart from '../Cart';
+import React from "react";
+import NavBar from "../NavBar";
+import Footer from "../Footer";
+import Cart from "../Cart";
 
 const Layout = ({ children }) => {
-
   return (
     <div>
       <NavBar />
-      <Cart open={false}/>
+      <div className="static bg-green-500">
+        <Cart open={false} />
+      </div>
       {children}
       <Footer />
     </div>
