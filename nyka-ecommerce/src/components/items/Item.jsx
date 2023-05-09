82d12b960f787ba9eceb32976e19de27d8d77011
Leandro <leandrofranco50@yahoo.com.ar>
2023-05-09 09:40:12 -0300

    commit for pull

--- nyka-ecommerce/src/components/items/Item.jsx
+++ nyka-ecommerce/src/components/items/Item.jsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 import React from "react";
 import { IconBagSvg } from "../Svgs";
 import img from "../../../public/Image.png";
+import Link from "next/link";
 
 const Item = ({ item }) => {
   return (
+    <Link href={`/item/${item.id}`}>
     <div>
       <div className="card w-80 bg-white shadow-xl">
         <figure className="">
@@ -13,7 +15,7 @@ const Item = ({ item }) => {
         </figure>
         <div className="flex flex-col gap-2 my-4 items-center text-center text-black">
           <h2 className="card-title text-base">{item.nombre}</h2>
-          <h3 className="card-title text-lg">{`$${item.precio}`}</h3>
+          <h3 className="card-title text-lg">${new Intl.NumberFormat("de-DE").format(item.precio)}</h3>
           <p className=" text-xs  text-gray-800 ">3 cuotas sin interés o 20%off en transferencia</p>
           <div className="card-actions">
             <button className="w-12 h-12 mt-[4px] flex items-center justify-center rounded-full bg-[#8D4925]">
@@ -23,6 +25,7 @@ const Item = ({ item }) => {
         </div>
       </div>
     </div>
+    </Link>
   );
 };
 
---
+++ nyka-ecommerce/src/pages/producto.js
@@ -0,0 +1,7 @@
+import ItemDetailContainer from '../components/ItemDetailContainer'
+
+function ItemDetailPage() {
+  return <ItemDetailContainer />
+}
+
+export default ItemDetailPage
