f02c9295cb344df932b06158a2a85f13bef3a4ac
Leandro <leandrofranco50@yahoo.com.ar>
2023-05-27 09:59:51 -0300

    Update index.js

--- nyka-ecommerce/src/pages/index.js
+++ nyka-ecommerce/src/pages/index.js
@@ -1,6 +1,6 @@
 import CardsContainer from "../components/cards/CardsContainer";
 import Carousel from "@/components/Carousel";
-import InstagramSection from "@/components/InstagramSection";
+import InstagramSection from "../components/InstagramSection";
 import Layout from "../components/layout/Layout";
 import PayInfo from "@/components/PayInfo";
 import Why from "../components/Why";
